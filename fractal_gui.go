a361c7204f4d624f5f11c8e74dab4068a47f79dc
Colin Maykish <colin.maykish@keeptruckin.com>
2021-04-01 17:41:42 -0400

    Simpler two color hues

--- fractal_gui.go
+++ fractal_gui.go
@@ -9,23 +9,41 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-const width = 500
-const height = 500
+const width = 1000
+const height = 1000
 
-var iterations = 1000
+var iterations = 2000
+var iterationGap = 1000
+
+var lightColor = uint32(0xFFFFFF)
+var darkColor = uint32(0x000000)
+
+func newColor(colorA, colorB uint32, hue float64) (uint8, uint8, uint8) {
+	var ra = uint8(colorA & (0xFF << 16) >> 16)
+	var ga = uint8(colorA & (0xFF << 8) >> 8)
+	var ba = uint8(colorA & 0xFF)
+	var rb = uint8(colorB & (0xFF << 16) >> 16)
+	var gb = uint8(colorB & (0xFF << 8) >> 8)
+	var bb = uint8(colorB & 0xFF)
+
+	return uint8(float64(rb-ra)*hue) + ra, uint8(float64(gb-ga)*hue) + ga, uint8(float64(bb-ba)*hue) + ba
+}
 
 func drawMandelbrot(renderer *sdl.Renderer, m *fractal_core.Mandelbrot) {
+	renderer.Clear()
+
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			var color = uint8(0)
+			renderer.SetDrawColor(0, 0, 0, 0xFF)
 
-			var cellValue = fractal_core.GetBuffer(m)[x][y]
+			var val = fractal_core.GetBuffer(m)[x][y]
+			var hue = fractal_core.GetHue(m)[x][y]
 
-			if cellValue < uint32(iterations) {
-				color = uint8(fractal_core.MapIntToFloat(int(cellValue), 0, iterations, 0, math.Pow(2, 8)-1))
+			if val < uint32(iterations) {
+				var r, g, b = newColor(darkColor, lightColor, hue)
+				renderer.SetDrawColor(r, g, b, 0x00)
 			}
 
-			renderer.SetDrawColor(color, color, color, 0xFF)
 			renderer.DrawPoint(int32(x), int32(y))
 		}
 	}
@@ -40,7 +58,7 @@ func main() {
 	defer sdl.Quit()
 
 	fmt.Println("Creating window")
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	window, err := sdl.CreateWindow("Fractal Interactive", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		width, height, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
@@ -52,8 +70,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Failed to create renderer: %s\n", err)
 	}
 	defer renderer.Destroy()
-
-	renderer.Clear()
 
 	// Create the Mandelbrot generator
 	m := fractal_core.Create(width, height, complex(-0.5, 0))
@@ -85,7 +101,6 @@ func main() {
 			case *sdl.MouseButtonEvent:
 				if event.GetType() == sdl.MOUSEBUTTONDOWN {
 					clickx, clicky, _ = sdl.GetMouseState()
-					fmt.Printf("click at %d, %d\n", clickx, clicky)
 					regen = true
 				}
 			}
@@ -96,13 +111,17 @@ func main() {
 
 			var realX = fractal_core.MapIntToFloat(int(clickx), 0, width, minx, maxx)
 			var realY = fractal_core.MapIntToFloat(int(clicky), 0, height, miny, maxy)
-			fmt.Printf("(%f + %fi)\n", realX, realY)
+			var op = "+"
+
+			if realY < 0 {
+				op = "-"
+			}
 
 			fractal_core.SetCenter(m, complex(realX, realY))
 
-			fractal_core.ScaleZoom(m, 10.0)
+			fractal_core.ScaleZoom(m, 5.0)
 
-			iterations += 1000
+			iterations += iterationGap
 
 			fractal_core.SetMaxIterations(m, iterations)
 
@@ -114,6 +133,7 @@ func main() {
 			window.UpdateSurface()
 
 			fmt.Println("Done!")
+			fmt.Printf("(%v %s %vi) at %vx\n", realX, op, math.Abs(realY), fractal_core.GetZoom(m))
 
 			regen = false
 		}
